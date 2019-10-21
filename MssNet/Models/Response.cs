8c977c0dc727bd598715123e0e485c190f29f5fd
Philipp Schönauer <philipp.schoenauer@hgv.it>
2019-10-21 10:58:24 +0200

    Replaced host url

--- MssNet/Models/Response.cs
+++ MssNet/Models/Response.cs
@@ -291,6 +291,8 @@ namespace MssNet.Models.Response
 
     public class Picture
     {
+        private string url;
+
         [XmlElement(ElementName = "copyright")]
         public string Copyright { get; set; }
 
@@ -299,9 +301,13 @@ namespace MssNet.Models.Response
 
         [XmlElement(ElementName = "title")]
         public string Title { get; set; }
-
+        
         [XmlElement(ElementName = "url")]
-        public string Url { get; set; }
+        public string Url
+        {
+            get => url;
+            set => url = value.Replace("www.easymailing.eu", "www.bookingsuedtirol.com");
+        }
     }
 
     public class Offer
